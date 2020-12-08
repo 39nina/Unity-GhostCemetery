5fcad474049a5eb9b4929718473d4e5bf2f38bfe
39nina <392mknn@gmail.com>
2020-12-08 18:32:38 +0900

    Add：フィールド、Player、ghost(enemy)の設置

---
+++ Assets/Scripts/GhostManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostManager : MonoBehaviour
+{
+    [SerializeField] GameObject player = default;
+    float distance;
+    Vector3 ghostPos;
+
+    private void Awake()
+    {
+        ghostPos = this.transform.position;
+        //Debug.Log(ghostPos);
+    }
+
+    private void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        Debug.Log(player.transform.position);
+        // プレイヤーとの距離を計測
+        distance = (player.transform.position - ghostPos).magnitude;
+        Debug.Log(distance);
+        Appear();
+    }
+
+    // 距離が一定以下になったら出現
+    void Appear()
+    {
+        if(distance <= 100)
+        {
+            this.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Rigidbody rb;
+    Animator animator;
+    float x;
+    float z;
+    float speed = 4.5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal") * speed;
+        z = Input.GetAxisRaw("Vertical") * -1 * speed;
+
+        Attack();
+    }
+
+    private void FixedUpdate()
+    {
+        Run();
+    }
+
+    // 移動時のアニメーション
+    void Run()
+    {
+        rb.velocity = new Vector3(x, 0, z);
+        animator.SetFloat("Speed", rb.velocity.magnitude);
+    }
+
+    // 攻撃時のアニメーション
+    void Attack()
+    {
+        if (Input.GetButtonDown("Attack"))
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    // 攻撃時にエフェクトを生成
+    void OnEventFx(GameObject InEffect)
+    {
+        GameObject newSpell = Instantiate(InEffect, this.transform);
+
+        Destroy(newSpell, 1.0f);
+    }
+}

4e190b8b5ceed97bd2f960cde479243f714c35a2
39nina <392mknn@gmail.com>
2020-12-11 00:36:54 +0900

    Add：近距離で○ボタンを押すとキャンドルに点灯する挙動を追加

---
+++ Assets/Scripts/CandleManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleManager : MonoBehaviour
+{
+    [SerializeField] GameObject candle_off = default;
+    [SerializeField] GameObject candle_on1 = default;
+    [SerializeField] GameObject candle_on2 = default;
+    [SerializeField] GameObject candle_on3 = default;
+    [SerializeField] GameObject Player = default;
+    Vector3 candlePos;
+    Vector3 playerPos;
+    float distance;
+
+    private void Start()
+    {
+        candlePos = this.gameObject.transform.position;
+    }
+
+    private void Update()
+    {
+        // プレイヤーとの距離を観測
+        playerPos = Player.transform.position;
+        distance = (candlePos - playerPos).magnitude;
+
+        LightCandle();
+    }
+
+    // 一定以下の距離で○ボタンを押すと点灯
+    void LightCandle()
+    {
+        if (distance <= 1.2f && Input.GetButtonDown("Light"))
+        {
+            candle_off.SetActive(false);
+            candle_on1.SetActive(true);
+            candle_on2.SetActive(true);
+            candle_on3.SetActive(true);
+        }
+    }
+}

db92e3fd6240c7f51be2e14ce3f41191c5468402
39nina <392mknn@gmail.com>
2020-12-16 15:29:48 +0900

    Fix：プレイヤーが点灯中に移動しないよう修正、移動中の停止も含めbehaviourに記述

---
+++ Assets/Scripts/PlayerAttackBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackBehaviour : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // 点灯アニメーションが連続して作動しないようにリセットする
+        animator.ResetTrigger("Attack");
+        // 攻撃中は動けないようにする
+        animator.GetComponent<PlayerManager>().StopMove();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // 点灯アニメーションが連続して作動しないようにリセットする
+        animator.ResetTrigger("Attack");
+        // 攻撃が終わったら動けるようにする
+        animator.GetComponent<PlayerManager>().StartMove();
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}

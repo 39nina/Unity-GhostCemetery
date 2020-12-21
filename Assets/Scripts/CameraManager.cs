d0ddf1b353b20b13c292d6304bdcfc8a9380b445
39nina <392mknn@gmail.com>
2020-12-21 14:36:44 +0900

    Fix：カメラの追従をスクリプトでの管理に修正

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] GameObject player = default;
+    Vector3 offset;
+
+    private void Start()
+    {
+        // プレイヤーとカメラの距離を計測
+        offset = this.GetComponent<Transform>().position - player.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        this.GetComponent<Transform>().position = player.transform.position + offset;
+    }
+}

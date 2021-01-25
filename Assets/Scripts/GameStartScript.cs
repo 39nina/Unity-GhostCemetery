67be4e80c9accac995916eae797c69b8e63420e2
39nina <392mknn@gmail.com>
2021-01-25 19:17:13 +0900

    Add：スタート画面追加

---
+++ Assets/Scripts/GameStartScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStartScript : MonoBehaviour
+{
+
+    // コントローラでスタートする用の関数
+    private void Update()
+    {
+        if (Input.GetButtonDown("Light"))
+        {
+            SceneManager.LoadScene("cemetery");
+        }
+    }
+
+    // 画面上をクリックした際にゲームスタートする用の関数
+    public void GameStartButton()
+    {
+        SceneManager.LoadScene("cemetery");
+    }
+}

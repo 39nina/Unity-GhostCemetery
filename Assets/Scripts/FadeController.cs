0b3e32ac69c5900e6625873cd8d6eb4f1d586b98
39nina <392mknn@gmail.com>
2021-02-03 19:48:38 +0900

    Add：ゾンビ登場時のBGM設定

---
+++ Assets/Scripts/FadeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeController : MonoBehaviour
+{
+    float fadeSpeed = 0.1f;
+    float alfa;　　// 透明度を管理する変数
+    public bool isFadeOut = true;
+    Image fadeImage;
+
+    void Start()
+    {
+        fadeImage = GetComponent<Image>();
+        alfa = fadeImage.color.a;
+    }
+
+    void Update()
+    {
+        Invoke("StartFadeOut", 6.4f);
+    }
+
+    void StartFadeOut()
+    {
+    if (isFadeOut)
+    {
+        alfa += fadeSpeed;
+        fadeImage.color = new Color(0, 0, 0, alfa);
+
+        // 完全に不透明になったらフェードインを終える
+        if (alfa >= 255)
+        {
+            isFadeOut = false;
+        }
+    }
+    }
+}

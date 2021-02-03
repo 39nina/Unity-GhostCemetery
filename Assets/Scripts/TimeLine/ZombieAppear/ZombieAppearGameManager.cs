4253456fe54044c01c69600e9c77a2478a323813
39nina <392mknn@gmail.com>
2021-02-03 14:06:33 +0900

    Add：ゾンビ登場シーンの動画作成

---
+++ Assets/Scripts/TimeLine/ZombieAppear/ZombieAppearGameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAppearGameManager : MonoBehaviour
+{
+    // ゾンビ登場シーン用のスクリプト
+
+    [SerializeField] GameObject EntranceBurnOut = default;
+    [SerializeField] GameObject EntranceLight = default;
+
+    private void Start()
+    {
+        Invoke("EntranceLighted", 0.0f);
+    }
+
+    void EntranceLighted()
+    {
+        EntranceLight.SetActive(true);
+        EntranceBurnOut.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/TimeLine/ZombieAppear/ZombieAppearManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAppearManager : MonoBehaviour
+{
+    public AudioClip AttackSE;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    public void PlayAttackSE()
+    {
+        audioSource.PlayOneShot(AttackSE);
+    }
+}
